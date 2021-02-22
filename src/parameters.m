434668c5ee4b998b4a7eff81421ae62495bb7bb6
fjctp <snchan20@yahoo.com>
2021-02-22 00:10:44 -0500

    added gyro low pass filter to parameter.m such that it is tunable.

--- src/parameters.m
+++ src/parameters.m
@@ -7,3 +7,9 @@ R = diag([0.1, 0.1, 0.1])*9.81;
 
 P0 = [1e-06 0 0;0 1e-06 0;0 0 1e-06];
 x0 = [0;0;0];
+
+lpfreqHz = 25;
+lpf = tf(1, [1/(lpfreqHz*2*pi), 1]);
+lpfDt = c2d(lpf, dt, 'tustin');
+
+[lpfDtNum, lpfDtDen] = tfdata(lpfDt);
