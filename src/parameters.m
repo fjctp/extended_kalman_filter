ddc5e07a6b744164f263c7559481cf751a5528db
fjctp <snchan20@yahoo.com>
2021-02-21 22:42:55 -0500

    updated the tune: Q

--- src/parameters.m
+++ src/parameters.m
@@ -1,7 +1,7 @@
 dt = 1/100;
 a = dt^2/2;
 
-pqr_dot_covar = [deg2rad(1), deg2rad(1), deg2rad(1)];
+pqr_dot_covar = [deg2rad(5), deg2rad(5), deg2rad(5)];
 Q = diag([a*pqr_dot_covar(1)*a, a*pqr_dot_covar(2)*a, a*pqr_dot_covar(3)*a]);
 R = diag([0.1, 0.1, 0.1])*9.81;
 
