e217bd8c6436d073fa988e0a730e6029aa64569f
fjctp <snchan20@yahoo.com>
2021-02-21 22:30:29 -0500

    separated the test from the model and fixed bugs in the test

--- src/parameters.m
+++ src/parameters.m
@@ -5,8 +5,5 @@ pqr_dot_covar = [deg2rad(1), deg2rad(1), deg2rad(1)];
 Q = diag([a*pqr_dot_covar(1)*a, a*pqr_dot_covar(2)*a, a*pqr_dot_covar(3)*a]);
 R = diag([0.1, 0.1, 0.1])*9.81;
 
-mdlName = 'kalman_filter';
-hws = get_param(mdlName, 'modelworkspace');
-hws.assignin('dt', dt);
-hws.assignin('Q', Q);
-hws.assignin('R', R);
+P0 = [1e-06 0 0;0 1e-06 0;0 0 1e-06];
+x0 = [0;0;0];
