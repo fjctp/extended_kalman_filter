f1adb5a5893a5c6b25480d11765adec2ad1e9dc0
fjctp <snchan20@yahoo.com>
2021-03-28 18:32:11 -0400

    reorganize: moved utils to under models

--- src/models/utils/plot/plot_results.m
+++ src/models/utils/plot/plot_results.m
@@ -9,7 +9,11 @@ absDiff = logsout.get('absDiff').Values;
 labels = ["roll","pitch","yaw"];
 figure;
 for ii = 1:3
-    subplot(4, 1, ii);
+    if ~exist('ax', 'var')
+        ax(1) = subplot(4, 1, ii);
+    else
+        ax(end+1) = subplot(4, 1, ii);
+    end
     hold on
     plot(truthEuler.Time, rad2deg(truthEuler.Data(:,ii)));
     plot(eulerEst.Time, rad2deg(eulerEst.Data(:,ii)));
@@ -18,12 +22,14 @@ for ii = 1:3
     legend('Truth', 'Est');
 end
 
-subplot(4, 1, 4);
+ax(end+1) = subplot(4, 1, 4);
 hold on
 plot(absDiff*rad2deg(1));
 grid on;
 ylabel('|Error (deg)|');
 legend('roll', 'pitch', 'yaw');
+
+linkaxes(ax, 'x');
 
 dd.diff.x = absDiff.Data(:,1)*rad2deg(1);
 dd.diff.y = absDiff.Data(:,2)*rad2deg(1);
